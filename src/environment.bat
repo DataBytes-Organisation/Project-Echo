a5a436326cf3879bf1cd7b35c3f0a275faad5faa
akudilcz <akudilczak@deakin.edu.au>
2022-11-20 17:13:24 +1100

    added libs supporting HTS-AT training loop locally

--- src/environment.bat
+++ src/environment.bat
@@ -1,21 +1,10 @@
 
 REM clean up cache and start from scratch
 call conda activate base
+
+REM uncomment these lines to perform clean install if you have issues
 REM call conda clean --all --yes
 REM call pip cache purge
-
-REM create the minimal environment
-call conda activate base
-call conda env remove -v -n dev_min
-call conda env create -v -f env_dev_min.yaml
-
-REM install minimum environment
-call conda activate dev_min
-call pip install --upgrade pip
-call pip install pipwin
-call pipwin refresh
-call pipwin install openexr
-call pip install -v -r env_dev_min.txt
 
 REM create the full dev environment
 call conda activate base
@@ -30,7 +19,5 @@ call pipwin refresh
 call pipwin install openexr
 call pip install -v -r env_dev.txt
 
-REM clone environment to make a scratch
+REM done return to base
 call conda activate base
-call conda env remove -v -n scratch
-call conda create --clone dev --name scratch
