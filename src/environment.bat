09fcc6ebecfbce7e119ec47f9d8917614cd4dc0d
stephankokkas <stephankokkas@gmail.com>
2022-11-29 21:02:29 +1100

    Uploaded new end files

--- src/environment.bat
+++ src/environment.bat
@@ -4,19 +4,6 @@ call conda activate base
 REM call conda clean --all --yes
 REM call pip cache purge
 
-REM create the minimal environment
-call conda activate base
-call conda env remove -v -n dev_min
-call conda env create -v -f env_dev_min.yaml
-
-REM install minimum environment
-call conda activate dev_min
-call pip install --upgrade pip
-call pip install pipwin
-call pipwin refresh
-call pipwin install openexr
-call pip install -v -r env_dev_min.txt
-
 REM create the full dev environment
 call conda activate base
 call conda env remove -v -n dev
@@ -29,8 +16,3 @@ call pip install pipwin
 call pipwin refresh
 call pipwin install openexr
 call pip install -v -r env_dev.txt
-
-REM clone environment to make a scratch
-call conda activate base
-call conda env remove -v -n scratch
-call conda create --clone dev --name scratch
