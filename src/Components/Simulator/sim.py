d911697e7bdea8e0a00762785637e3d8c52453f6
Stephan C Kokkas <stephankokkas@gmail.com>
2023-03-23 15:12:41 +1100

    Revert "ValueError: math domain error"
    This reverts commit 74eb760ab91fc2dc8512fb8e80e854d0b8737a6d.

--- src/Components/Simulator/sim.py
+++ src/Components/Simulator/sim.py
@@ -6,7 +6,7 @@ import networkx as nx
 import matplotlib.pyplot as plt
 from event_generator import Event
 from geopy.distance import distance
-from math import radians, sin, cos, sqrt, asin, degrees, pi, atan2
+from math import radians, sin, cos, sqrt, asin, degrees, pi
 import matplotlib.pyplot as plt
 import folium
 from folium.plugins import MarkerCluster
@@ -58,42 +58,69 @@ class Simulator():
             print("Not enough TDOA data to triangulate event")
             return
 
-        self._triangulate(toda_data, event_lat, event_long)
+        print(self._triangulate(toda_data, event_lat, event_long))
         # error = distance((event_lat, event_long), (event_lat_est, event_long_est)).m
         # print(f"Triangulation error: {error} meters")
 
     def _triangulate(self, tdoa_data: list[tuple[float, float, float]], event_lat, event_long) -> tuple[float, float]:
-        def get_intersections(latitudes, longitudes, radii):
-            R = 6371  # radius of the Earth in km
-            intersections = []
-            for i in range(len(latitudes)):
-                for j in range(i+1, len(latitudes)):
-                    lat1, lon1, r1 = latitudes[i], longitudes[i], radii[i]
-                    lat2, lon2, r2 = latitudes[j], longitudes[j], radii[j]
-                    d = math.acos(math.sin(lat1)*math.sin(lat2) + math.cos(lat1)*math.cos(lat2)*math.cos(lon1-lon2)) * R
-                    if d <= r1 + r2:
-                        # circles intercept, calculate intersection points
-                        a = (r1**2 - r2**2 + d**2) / (2*d)
-                        h = math.sqrt(r1**2 - a**2)
-                        lat3 = math.asin(math.sin(lat1)*math.cos(h/d) + math.cos(lat1)*math.sin(h/d)*math.cos(lon1-lon2))
-                        lon3 = lon1 + math.atan2(math.sin(lon2-lon1)*math.cos(lat2), math.cos(lat1)*math.sin(lat2) - math.sin(lat1)*math.cos(lat2)*math.cos(lon2-lon1))
-                        intersections.append((lat3, lon3))
-            if len(intersections) > 0:
-                print("Intersections found at:")
-                for i in range(len(intersections)):
-                    print(f"Intersection {i+1}: latitude={intersections[i][0]}, longitude={intersections[i][1]}")
-            else:
-                print("No intersections found.")
+        def distance(lat1, lon1, lat2, lon2):
+            # Convert latitude and longitude to radians
+            lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+            # Haversine formula for calculating distance between two points on a sphere
+            dlon = lon2 - lon1 
+            dlat = lat2 - lat1 
+            a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2
+            c = 2 * asin(sqrt(a)) 
+            distance_meters = R * c
+            return distance_meters
 
-        c = 343 # speed of sound in meters/second
+        def trilateration(lat1, lon1, r1, lat2, lon2, r2, lat3, lon3, r3):
+            # Convert latitudes and longitudes to meters
+            x1, y1 = distance(lat1, lon1, lat1, lon1), distance(lat1, lon1, lat1, lon1)
+            x2, y2 = distance(lat2, lon2, lat1, lon1), distance(lat2, lon2, lat1, lon1)
+            x3, y3 = distance(lat3, lon3, lat1, lon1), distance(lat3, lon3, lat1, lon1)
+            A = 2 * x2 - 2 * x1
+            B = 2 * y2 - 2 * y1
+            C = r1**2 - r2**2 - x1**2 + x2**2 - y1**2 + y2**2
+            D = 2 * x3 - 2 * x2
+            E = 2 * y3 - 2 * y2
+            F = r2**2 - r3**2 - x2**2 + x3**2 - y2**2 + y3**2
+            x = (C*E - F*B) / (E*A - B*D)
+            y = (C*D - A*F) / (B*D - A*E)
+            # Convert the intersection point from meters to latitude and longitude
+            intersection_lat = degrees(y / R)
+            intersection_lon = degrees(x / (R * cos(radians(intersection_lat))))
+            return intersection_lat, intersection_lon
+        
+        c = 343*2 # speed of sound in meters/second
         R = 6371000  # radius of the Earth in meters
 
         latitudes = [tdoa_data[2][0], tdoa_data[1][0], tdoa_data[0][0]]
         longitudes = [tdoa_data[2][1], tdoa_data[1][1], tdoa_data[0][1]]
         radii = [c * tdoa_data[2][2], c * tdoa_data[1][2], c * tdoa_data[0][2]]
         self.map_intersections(latitudes, longitudes, radii, event_lat, event_long)
-        get_intersections(latitudes, longitudes, radii)
 
+        for i in range(len(latitudes)):
+            for j in range(i+1, len(latitudes)):
+                lat1, lon1, r1 = latitudes[i], longitudes[i], radii[i]
+                lat2, lon2, r2 = latitudes[j], longitudes[j], radii[j]
+                d = sqrt((lat2-lat1)**2 + (lon2-lon1)**2)
+                if d < r1 + r2:  # if the circles intersect
+                    for k in range(j+1, len(latitudes)):
+                        lat3, lon3, r3 = latitudes[k], longitudes[k], radii[k]
+                        d1 = sqrt((lat3-lat1)**2 + (lon3-lon1)**2)
+                        d2 = sqrt((lat3-lat2)**2 + (lon3-lon2)**2)
+                        if d1 < r1 + r3 and d2 < r2 + r3: # if all three circles intersect
+                            lat1, lon1, r1 = latitudes[0], longitudes[0], radii[0]
+                            lat2, lon2, r2 = latitudes[1], longitudes[1], radii[1]
+                            lat3, lon3, r3 = latitudes[2], longitudes[2], radii[2]
+
+                            intersection_lat, intersection_lon = trilateration(lat1, lon1, r1, lat2, lon2, r2, lat3, lon3, r3)
+
+                            if intersection_lat is not None and intersection_lon is not None:
+                                print(f'The circles intersect at the point {(intersection_lat, intersection_lon)}')
+                            else:
+                                print('The circles do not intersect')
 
     
     def map_intersections(self, latitudes, longitudes, radii, event_lat, event_long):
