a215e3bb460e94f4a8f7abd9b512998b0818c676
Stephan C Kokkas <stephankokkas@gmail.com>
2023-03-25 14:52:41 +1100

    init commit

--- src/Components/Simulator/entities/microphone.py
+++ src/Components/Simulator/entities/microphone.py
@@ -5,19 +5,26 @@
 
 import entities.entity
 import uuid
+import datetime
 
 class MicrophoneStation(entities.entity.Entity):
     def __init__(self, lla=(0,0,10.0)) -> None:
         self.name = "OnField-Microphone"
         self.unique_identifier = str(uuid.uuid4())
-        self.time_delay_seconds: float
+
+        self.event_timestamp: datetime
+        self.TRIGGERED: bool = False
         super(MicrophoneStation, self).__init__(lla)
 
-    def set_time_delay(self, tds) -> None:
-        self.time_delay_seconds = tds
-
-    def get_time_delay(self) -> None:
-        return self.time_delay_seconds
+    def set_trigger_event_time(self, TT: datetime) -> None:
+        self.event_timestamp = TT
+        self.set_trigger()
+    
+    def get_trigger_event_time(self) -> datetime:
+        return self.event_timestamp
+    
+    def set_trigger(self) -> None:
+        self.TRIGGERED = True
         
 
         
--- src/Components/Simulator/simulator.py
+++ src/Components/Simulator/simulator.py
@@ -1,4 +1,7 @@
 import json
+from datetime import datetime as dt
+import datetime
+import networkx as nx
 import uuid
 import os
 import networkx as nx
@@ -18,6 +21,7 @@ class Simulator():
         self.NODE_THRESHOLD = 0.008
         self.SIMULATED_OTWAYS_MAP = Otways()
         self.graph = None
+        self.c = 343 # Speed of sound in m/s
         
     def _connect_mics(self) -> None:
         self.graph = nx.Graph()
@@ -38,95 +42,56 @@ class Simulator():
         plt.savefig(os.path.join(os.getcwd(),'src/Components/Simulator/outputs/mic_node_graph.png'))
         if plt_noodes: plt.show()
 
-    def triangulate_event(self, lla) -> None:
-        print(f'\nEvent occured at Lat {lla[0]}, and Long {lla[1]}\n')
-        event_lat = lla[0]
-        event_long = lla[1]
+    def simulate_event_occuring(self, animal_obect: Animal) -> None:
+        print(f'\nEvent occured at Lat, Long pair [{animal_obect.getLLA()[0]}, {animal_obect.getLLA()[1]}] at time: {animal_obect.get_sound_production_time()}\n')
 
-        toda_data = []
-        distances = []
+        def find_closest_mics(graph, event_lat, event_long, n):
+            mics_distances = []
+            for mic in graph.nodes():
+                mic_lat, mic_long = mic.getLLA()[0], mic.getLLA()[1]
+                distance = ((mic_lat - event_lat) ** 2 + (mic_long - event_long) ** 2) ** 0.5
+                mics_distances.append((mic, distance))
+            mics_distances.sort(key=lambda x: x[1])
+            return [mic for mic, _ in mics_distances[:n]]
 
-        for mic in self.SIMULATED_OTWAYS_MAP.microphones:
-            mic_distance = distance((event_lat, event_long), (mic.getLLA()[0], mic.getLLA()[1])).m
-            distances.append((mic, mic_distance))
+        mics_around_event = find_closest_mics(self.graph, animal_obect.getLLA()[0], animal_obect.getLLA()[1], 4)
+        for mic in mics_around_event:
+            mic.set_trigger_event_time(animal_obect.get_sound_production_time() - datetime.timedelta(seconds=mic.distance(animal_obect)/self.c))
 
-        sorted_mics = sorted(distances, key=lambda x: x[1])
-
-        for mic, dist in sorted_mics[:3]:
-            c = 343
-            time_diff = dist / c
-            mic.set_time_delay(time_diff)
-            print(f"Microphone triggered at lat: {mic.getLLA()[0]}, long: {mic.getLLA()[1]} with time diff of {time_diff} seconds")
-            toda_data.append(mic)
-
-        if len(toda_data) < 3:
-            print("Not enough TDOA data to triangulate event")
-            return
         
-        # Show mic souond radius on map as well as event location and output map
-        marker_cluster = MarkerCluster().add_to(self.SIMULATED_OTWAYS_MAP.folium_map)
+        def find_triggered_mics(graph):
+            triggered_mics = []
+            for mic in graph.nodes():
+                if mic.TRIGGERED:
+                    triggered_mics.append(mic)
+            return triggered_mics
 
-        for mic in toda_data:
-            folium.CircleMarker(
-                location=[mic.getLLA()[0], mic.getLLA()[1]],
-                radius=mic.get_time_delay()*c,
-                color='black',
-                fill=False,
-                dash_array='10'
-            ).add_to(marker_cluster)
-
-        folium.Marker(location=[event_lat, event_long], icon=folium.Icon(icon="paw", prefix='fa', color="red"), popup="Event Location").add_to(self.SIMULATED_OTWAYS_MAP.folium_map)
-        self.SIMULATED_OTWAYS_MAP.folium_map.save(os.path.join(os.getcwd(),'src/Components/Simulator/outputs/map_event_detection.html'))
+        for mic in find_triggered_mics(self.graph):
+            print(mic.get_trigger_event_time())
+        input()
         
-        predicted_lla = self._triangulate(toda_data)
-        if predicted_lla is not None:
-            error = distance((event_lat, event_long), (predicted_lla[0], predicted_lla[1])).m
-            print(f"\nTriangulation error: {round(error, 2)} meters")
-            self.map_intersections(predicted_lla)
-            self.broadcast_message(predicted_lla, toda_data)
-        else:
-            print('\nInsufficient trilaterate')
+        # marker_cluster = MarkerCluster().add_to(self.SIMULATED_OTWAYS_MAP.folium_map)
 
-    def _triangulate(self, tdoa_data) -> tuple[float, float]:
-        def trilaterate(p1, r1, p2, r2, p3, r3):
-            # Calculate relative positions of point 2 and point 3
-            ex = (p2 - p1) / np.linalg.norm(p2 - p1)
-            i = np.dot(ex, p3 - p1)
-            ey = (p3 - p1 - i * ex) / np.linalg.norm(p3 - p1 - i * ex)
-            ez = np.cross(ex, ey)
+        # for mic in toda_data:
+        #     folium.CircleMarker(
+        #         location=[mic.getLLA()[0], mic.getLLA()[1]],
+        #         radius=mic.get_time_delay()*c,
+        #         color='black',
+        #         fill=False,
+        #         dash_array='10'
+        #     ).add_to(marker_cluster)
 
-            # Calculate the distances
-            d = np.linalg.norm(p2 - p1)
-            j = np.dot(ey, p3 - p1)
-
-            # Calculate the position of the intersection point
-            x = (r1 ** 2 - r2 ** 2 + d ** 2) / (2 * d)
-            y = (r1 ** 2 - r3 ** 2 + i ** 2 + j ** 2) / (2 * j) - (i / j) * x
-
-            # Calculate the z-coordinate, if possible
-            z_squared = r1 ** 2 - x ** 2 - y ** 2
-            if z_squared <= 0:
-                return None  # No intersection
-            z = np.sqrt(z_squared)
-
-            # Calculate the coordinates
-            p = p1 + x * ex + y * ey + z * ez
-            return p
+        # folium.Marker(location=[event_lat, event_long], icon=folium.Icon(icon="paw", prefix='fa', color="red"), popup="Event Location").add_to(self.SIMULATED_OTWAYS_MAP.folium_map)
+        # self.SIMULATED_OTWAYS_MAP.folium_map.save(os.path.join(os.getcwd(),'src/Components/Simulator/outputs/map_event_detection.html'))
         
-        c = 343 # Speed of sound in m/s through air
-
-        _p_ = []
-        _r_ = []
-        for mic in tdoa_data:
-            _p_.append(np.array(mic.getECEF()))
-            _r_.append((mic.get_time_delay() * c) * 1.001) # add weight to radius
-
-        intersection = trilaterate(_p_[0], _r_[0], _p_[1], _r_[1], _p_[2], _r_[2])
-
-        if intersection is not None:
-            predicted_animal = Animal()
-            predicted_animal.setECEF(intersection)
-            return predicted_animal.getLLA()
+        # predicted_lla = self._triangulate(toda_data)
+        # if predicted_lla is not None:
+        #     error = distance((event_lat, event_long), (predicted_lla[0], predicted_lla[1])).m
+        #     print(f"\nTriangulation error: {round(error, 2)} meters")
+        #     self.map_intersections(predicted_lla)
+        #     self.broadcast_message(predicted_lla, toda_data)
+        # else:
+        #     print('\nInsufficient trilaterate')
     
     def map_intersections(self, predicted_lla) -> None:
         folium.Marker(location=[predicted_lla[0] + 0.0001, predicted_lla[1] + 0.0001], icon=folium.Icon(icon="signal-stream", color="green"), popup="Predicted Location", icon_offset=(0, 0)).add_to(self.SIMULATED_OTWAYS_MAP.folium_map)
@@ -151,7 +116,7 @@ if __name__ == "__main__":
     ECHO_SIMULATOR._connect_mics()
     ECHO_SIMULATOR._draw_node_graph()
 
-    ECHO_SIMULATOR.triangulate_event(Animal().getLLA())
+    ECHO_SIMULATOR.simulate_event_occuring(Animal())
 
         
         
