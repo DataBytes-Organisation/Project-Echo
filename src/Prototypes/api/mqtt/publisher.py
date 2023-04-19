484a2b1af85dfdca3f63738ebb49f47da1a42e8c
Tony Zhai <tzhai@deakin.edu.au>
2023-04-19 14:32:25 +1000

    working MQTT demo

--- src/Prototypes/api/mqtt/publisher.py
+++ src/Prototypes/api/mqtt/publisher.py
@@ -1,5 +1,12 @@
 import paho.mqtt.client as paho
 import time
+import json
+
+
+f = open('movements.json')
+data = json.load(f)
+
+
 
 def on_publish(client, userdata, mid):
     print("mid: "+str(mid))
@@ -9,8 +16,13 @@ client.on_publish = on_publish
 client.connect('broker.mqttdashboard.com', 1883)
 client.loop_start()
 
+i = 0
 while True:
-    temperature = 15
-    (rc, mid) = client.publish('projectecho/1', str(temperature), qos=1)
-    print(temperature)
+    MQTT_MSG = json.dumps(data[i])
+
+    (rc, mid) = client.publish('projectecho/1', MQTT_MSG, qos=1)
+    print(MQTT_MSG)
     time.sleep(5)
+    i += 1
+    
+    
--- src/Prototypes/api/mqtt/subscriber.py
+++ src/Prototypes/api/mqtt/subscriber.py
@@ -1,10 +1,31 @@
 import paho.mqtt.client as paho
+import pymongo
+import json
+import datetime
+import dotenv
+import os
+
+
+
+dotenv.load_dotenv()
+
+id = os.getenv("DB_USERNAME")
+password = os.getenv("DB_PASSWORD")
+connection_string=f"mongodb+srv://{id}:{password}@cluster0.gu2idc8.mongodb.net/test"
+myclient = pymongo.MongoClient(connection_string)
+mydb = myclient["mydatabase"]
+print(myclient.list_database_names())
+
 
 def on_subscribe(client, userdata, mid, granted_qos):
     print("Subscribed: "+str(mid)+" "+str(granted_qos))
 
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))    
+    mycol = mydb["movements"]
+    json_object = json.loads(msg.payload)
+    print(json_object)
+    mycol.insert_one(json_object)
 
 client = paho.Client()
 client.on_subscribe = on_subscribe
