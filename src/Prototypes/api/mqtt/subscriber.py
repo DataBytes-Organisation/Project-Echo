88c55b5f92d0c5d5142d832f3a972e00af06a498
Tony Zhai <tzhai@deakin.edu.au>
2023-04-23 08:24:16 +1000

    fix gitignore

--- src/Prototypes/api/mqtt/subscriber.py
+++ src/Prototypes/api/mqtt/subscriber.py
@@ -11,18 +11,19 @@ dotenv.load_dotenv()
 
 id = os.getenv("DB_USERNAME")
 password = os.getenv("DB_PASSWORD")
-connection_string=f"mongodb+srv://{id}:{password}@cluster0.gu2idc8.mongodb.net/test"
+connection_string=f"mongodb+srv://projectechodeakin:YjGHDjNsEijhytYG@cluster0.gu2idc8.mongodb.net/test"
 myclient = pymongo.MongoClient(connection_string)
 mydb = myclient["mydatabase"]
 print(myclient.list_database_names())
-
+mydb["events"].drop()
+mycol = mydb["events"]
 
 def on_subscribe(client, userdata, mid, granted_qos):
     print("Subscribed: "+str(mid)+" "+str(granted_qos))
 
 def on_message(client, userdata, msg):
     print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))    
-    mycol = mydb["movements"]
+    
     json_object = json.loads(msg.payload)
     print(json_object)
     mycol.insert_one(json_object)
