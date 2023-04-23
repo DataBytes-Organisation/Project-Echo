721380d8c6b17f2cbcdc71dc4496eda270c0b70c
akudilcz <akudilcz@users.noreply.github.com>
2023-04-23 21:45:07 +1000

    fixed to only ask once for GCP credentials

--- src/Components/Engine/echo_engine.sh
+++ src/Components/Engine/echo_engine.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-gcloud auth application-default login
+FILE=/root/.config/gcloud/application_default_credentials.json
+if [ -f "$FILE" ]; then
+    echo "$FILE exists so skipping gcloud authentication"
+else 
+    echo "$FILE does not exist."
+    gcloud auth application-default login
+fi
 
 python echo_engine.py
-
