a8034819bd29e1e4c75fe11f22ba4565acfa1b21
Stephan C Kokkas <stephankokkas@gmail.com>
2023-04-23 20:34:21 +1000

    Preping for docker
    docker files ready to go but not tested and GCP not optimised for operating in a docker container

--- src/Components/Simulator/control_sim.py
+++ src/Components/Simulator/control_sim.py
@@ -5,4 +5,5 @@ import time
 MQTT_BROKER_URL = "localhost"
 MQTT_BROKER_PORT = 1883
 
+publish.single("Simulator_Controls", "Start", hostname=MQTT_BROKER_URL, port=MQTT_BROKER_PORT)
 publish.single("Simulator_Controls", "Stop", hostname=MQTT_BROKER_URL, port=MQTT_BROKER_PORT)
