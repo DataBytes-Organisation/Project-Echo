53c5081b69edeedbeebe42da1c40c75f9ac8626f
qcooke <q.cooke@deakin.edu.au>
2023-04-30 12:00:46 +1000

    rebasing

--- src/Prototypes/hmi/ui/server.js
+++ src/Prototypes/hmi/ui/server.js
@@ -7,8 +7,8 @@ const port = 8080;
 // serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-const serveIndex = require('serve-index'); 
-app.use('/images/bio', serveIndex(express.static(path.join(__dirname, '/images/bio'))));
+//const serveIndex = require('serve-index'); 
+//app.use('/images/bio', serveIndex(express.static(path.join(__dirname, '/images/bio'))));
 
 // start the server
 app.listen(port, () => {
