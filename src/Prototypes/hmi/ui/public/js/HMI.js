742fe5c81d0feb22b84317e91cdc5eba6fbb124b
qcooke <q.cooke@deakin.edu.au>
2023-04-30 12:00:15 +1000

    rebasing

--- src/Prototypes/hmi/ui/public/js/HMI.js
+++ src/Prototypes/hmi/ui/public/js/HMI.js
@@ -1,6 +1,8 @@
 "use strict";
 
 import { getAudioTestString } from "./HMI-utils.js";
+import { retrieveTruthEventsInTimeRange } from "./routes.js";
+import data from "./sample_data.json" assert { type: 'json' };
 
 var markups = ["elephant.png", "monkey.png", "tiger.png"];
 
@@ -12,9 +14,24 @@ var statuses = [
   "invasive",
 ];
 
+function matchStatus(status){
+  if (status == "least concern"){
+    return "normal";
+  }
+  else{
+    return status;
+  }
+}
+
 var vocalizedLayers = []
 
 var animalTypes = ["mammal", "bird", "amphibian", "reptile", "insect"];
+
+var sample_data = data.data;
+
+var animal_data = []
+
+export var animal_toggled = false;
 
 //For Demo purposes only
 //This plays an awful quailty audio test string
@@ -22,23 +39,100 @@ document.addEventListener('click', function() {
   //playAudioString(getAudioTestString());  
 });
 
+
 export function initialiseHMI(hmiState) {
   console.log(`initialising`);
-
   createBasemap(hmiState);
-
-  //addWildlifeLayers(hmiState);
+  console.log("Get sample element from document: ", document.getElementById("menuPanel"))
   addTruthLayers(hmiState);
+  addVocalisationLayers(hmiState);
 
-  addTruthFeatures(hmiState);
+  addAllTruthFeatures(hmiState);
+
+  createMapClickEvent(hmiState);
   //addVocalizedFeatures(hmiState); !!! Remove and work on in next Update
   addmicrophones(hmiState);
-  //queueSimUpdate(hmiState);
+  queueSimUpdate(hmiState);
   //simulateData(hmiState);
 }
 
 function updateFilters(){
   
+}
+
+export function updateAnimalMovementLayerFromPastData(hmiState, results){
+
+  clearAllTruthLayers();
+
+  hmiState.movementEvents = [];
+
+  for (let data of results) {
+    hmiState.movementEvents.push(convertJSONtoAnimalMovementEvent(hmiState, data));
+  }
+
+  addAllTruthFeatures(hmiState);
+}
+
+export function updateAnimalMovementLayerFromLiveData(hmiState, results){
+  let newMovementEvents = [];
+
+  for (let data of results) {
+    let event = convertJSONtoAnimalMovementEvent(hmiState, data);
+    hmiState.movementEvents.push(event);
+    newMovementEvents.push(event);
+  }
+
+  addNewTruthFeatures(hmiState, newMovementEvents);
+}
+
+export function resetWildlifeLayers(hmiState){
+  hmiState.vocalizationEvents = [];
+  hmiState.movementEvents = [];
+  clearAllVocalisationLayers();
+  clearAllTruthLayers();
+}
+
+export function clearAllVocalisationLayers(){
+  for (let stat of statuses) {
+    for (let animalType of animalTypes) {
+      let nextName = stat + "_" + animalType;
+      let layer = findMapLayerWithName(hmiState, nextName);
+      layer.getSource().clear();
+    }
+  }
+}
+
+export function clearAllTruthLayers(){
+  for (let stat of statuses) {
+    for (let animalType of animalTypes) {
+      let nextName = stat + "_" + animalType + "_truth";
+      let layer = findMapLayerWithName(hmiState, nextName);
+      layer.getSource().clear();
+    }
+  }
+}
+
+export function convertJSONtoAnimalMovementEvent(hmiState, data){
+    let movementEvent = {};
+
+    movementEvent.animalId = data.animalId;
+    movementEvent.eventId = "M_" + hmiState.truthEventId;
+
+    hmiState.truthEventId = hmiState.truthEventId + 1;
+
+    movementEvent.timestamp = hmiState.currentTime;
+    movementEvent.speciesScientificName = data.species.toLowerCase();
+    movementEvent.speciesIdentificationConfidence = 100.0;
+    movementEvent.locationLat = data.animalTrueLLA[0];
+    movementEvent.locationLon = data.animalTrueLLA[1];
+    movementEvent.locationConfidence = 100.0;
+    movementEvent.animalType = data.type.toLowerCase();
+    movementEvent.animalStatus = matchStatus(data.status.toLowerCase());
+    movementEvent.animalDiet = data.diet.toLowerCase();
+
+    console.log("Movement Event:", movementEvent);
+
+    return movementEvent;
 }
 
 function playAudioString(audioDataString) {
@@ -110,8 +204,8 @@ export function updateLayers(filterState)  {
 /* Add Vocalized Layers, work on in next branch
 function addVocalizedLayers(hmiState) {
   console.log("addVocalizedLayers called.")
-  console.log("voc locs", hmiState.vocalizationLocations);
-  hmiState.vocalizationLocations.forEach((entry) => {
+  console.log("voc locs", hmiState.vocalizationEvents);
+  hmiState.vocalizationEvents.forEach((entry) => {
     console.log("Vocalization Found")
     var vocalization = new ol.Feature({
       geometry: new ol.geom.Point(
@@ -162,18 +256,19 @@ function printTest(vocalizedLayers){
 }
 */
 
-function addTruthFeatures(hmiState) {
+function addAllTruthFeatures(hmiState) {
   //console.log("addTruthLayers called.")
-  //console.log("Truth locs", hmiState.trueLocations);
-  for (let entry of hmiState.trueLocations) {
+  //console.log("Truth locs", hmiState.movementEvents);
+  for (let entry of hmiState.movementEvents) {
     //console.log("True location found!:  ")
     var trueLocation = new ol.Feature({
       geometry: new ol.geom.Point(
         ol.proj.fromLonLat([entry.locationLon,entry.locationLat])
       ),
-        name: 'trueLocation',
+        name: 'trueLocation_' + entry.speciesScientificName,
         animalType: entry.animalType,
-        animalStatus: entry.animalStatus
+        animalStatus: entry.animalStatus,
+        animalSpecies: entry.speciesScientificName 
     });
       //console.log(entry.locationLon, " ", entry.locationLat)
     
@@ -186,6 +281,47 @@ function addTruthFeatures(hmiState) {
         }),
     })
     trueLocation.setStyle(trueIcon);
+    trueLocation.setId(entry.eventId);
+
+    let layerName = deriveTruthLayerName(entry.animalStatus,entry.animalType);
+    let layer = findMapLayerWithName(hmiState, layerName);
+    let layerSource = layer.getSource();
+
+    console.log(layerName);
+    console.log("animal type: ", entry.speciesScientificName);
+
+    layerSource.addFeature(trueLocation);
+    layer.getSource().changed();
+    layer.changed();
+  }
+}
+
+function addNewTruthFeatures(hmiState, events) {
+  //console.log("addTruthLayers called.")
+  //console.log("Truth locs", hmiState.movementEvents);
+  for (let entry of events) {
+    //console.log("True location found!:  ")
+    var trueLocation = new ol.Feature({
+      geometry: new ol.geom.Point(
+        ol.proj.fromLonLat([entry.locationLon, entry.locationLat])
+      ),
+        name: 'trueLocation_' + entry.speciesScientificName,
+        animalType: entry.animalType,
+        animalStatus: entry.animalStatus,
+        animalSpecies: entry.speciesScientificName 
+    });
+      //console.log(entry.locationLon, " ", entry.locationLat)
+    
+    var trueIcon = new ol.style.Style({
+        image: new ol.style.Icon({
+          src: `./../images/${entry.animalType+"-"+entry.animalStatus}.png`,
+          anchor: [0.5, 1],
+          scale: 0.75,
+          className: 'true-icon'
+        }),
+    })
+    trueLocation.setStyle(trueIcon);
+    trueLocation.setId(entry.eventId);
 
     let layerName = deriveTruthLayerName(entry.animalStatus,entry.animalType);
     let layer = findMapLayerWithName(hmiState, layerName);
@@ -224,8 +360,8 @@ function addmicrophones(hmiState) {
   });
 
   //console.log("mics: ", mics);
-  addVectorLayerTopDown(hmiState, "micLayer");
-  let layer = findMapLayerWithName(hmiState, "micLayer");
+  addVectorLayerTopDown(hmiState, "mic_layer");
+  let layer = findMapLayerWithName(hmiState, "mic_layer");
   let layerSource = layer.getSource();
   layerSource.addFeatures(mics);
   layer.getSource().changed();
@@ -233,12 +369,13 @@ function addmicrophones(hmiState) {
 }
 
 export function showMics(hmiState){
-  let layer = findMapLayerWithName(hmiState, "micLayer");
+  let layer = findMapLayerWithName(hmiState, "mic_layer");
   let layerSource = layer.getSource();
   layer.setVisible(true);
 }
+
 export function hideMics(hmiState){
-  let layer = findMapLayerWithName(hmiState, "micLayer");
+  let layer = findMapLayerWithName(hmiState, "mic_layer");
   let layerSource = layer.getSource();
   layer.setVisible(false);
 }
@@ -252,7 +389,7 @@ function findMapLayerWithName(hmiState, name) {
 
     for (let i = 0; i < mapLayers.getLength(); ++i) {
       let currentLayer = mapLayers.item(i);
-      if (currentLayer.get("name") == name) {
+      if (currentLayer.get("name") == name.toLowerCase()) {
         return currentLayer;
       }
     }
@@ -286,7 +423,7 @@ function addVectorLayerToBasemap(hmiState, layerName, zIndex) {
   }
 }
 
-function addWildlifeLayers(hmiState) {
+function addVocalisationLayers(hmiState) {
   // Wildlife layers
   // Invasive, Normal, Near-Threatened, Vulnerable, Endangered
   // Mammal, Reptile, Predator, Bird, Amphibian
@@ -336,7 +473,7 @@ function createBasemap(hmiState) {
         name: "mapTileLayer",
         source: new ol.source.XYZ({
           url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-          maxZoom: 19,
+          maxZoom: 18,
         }),
       }),
     ],
@@ -350,6 +487,78 @@ function createBasemap(hmiState) {
   return basemap;
 }
 
+function createMapClickEvent(hmiState){
+  hmiState.basemap.on("click", function (evt) {
+    const feature = hmiState.basemap.forEachFeatureAtPixel(evt.pixel, function (feature) {
+      return feature;
+    });
+    if (feature){
+      let values = feature.getProperties();
+      if (values.animalSpecies){
+          var result = sample_data.find(({ common }) => common.toUpperCase() === values.animalSpecies.toUpperCase())
+          if (result) {
+            animal_data = result;
+            document.getElementById("desc_name").innerText = result.common;
+            document.getElementById("desc_species").innerText = result.species;
+            document.getElementById("desc_summary").innerText = result.summary;
+            document.getElementById("desc_img").src = "../../images/bio/" + result.common + ".png";
+            let summary = document.getElementById("desc_details");
+            summary.innerHTML = '';
+            result.description.forEach(content => {
+              var p = document.createElement('p');
+              p.className = "desc_ul";
+              p.innerText = content;
+              summary.appendChild(p);
+            })
+          animal_toggled = true;
+          }
+
+      }
+    }
+  });
+}
+
+export function MapOpenNav() {
+  if (animal_toggled){
+    document.getElementById("menuPanel").style.width = "30%";
+  }
+}
+
+export function MapCloseNav() {
+  document.getElementById("menuPanel").style.width = "0";
+  animal_toggled = false;
+}
+
+function updateTruthEvents(hmiState){
+  retrieveTruthEventsInTimeRange(hmiState.previousUpdateTime, hmiState.currentTime).then((res) => {
+    updateAnimalMovementLayerFromLiveData(hmiState, res.data);
+    //TODO also update vocalisation layer here.
+  })
+}
+
+function purgeTruthEvents(hmiState){
+  let persistEvents = [];
+  //console.log("purging");
+
+  //console.log(hmiState.liveEventCutoff);
+  for(let event of hmiState.movementEvents){
+    //console.log(event);
+
+    if(hmiState.liveEventCutoff > event.timestamp){
+      let layerName = deriveTruthLayerName(event.animalStatus, event.animalType);
+      let layer = findMapLayerWithName(hmiState, layerName);
+      const featureToPurge = layer.getSource().getFeatureById(event.eventId);
+      //console.log(featureToPurge);
+      layer.getSource().removeFeature(featureToPurge);
+    }
+    else{
+      persistEvents.push(event);
+    }
+  }
+
+  hmiState.movementEvents = persistEvents;
+}
+
 function simulateData(hmiState) {
 
   queueSimUpdate(hmiState);
@@ -358,12 +567,19 @@ function simulateData(hmiState) {
 let simUpdateTimeout = null;
 
 function queueSimUpdate(hmiState) {
+  if(hmiState.liveMode){
+    hmiState.currentTime = Math.floor((Date.now() - hmiState.timeOffset - hmiState.simUpdateDelay) / 1000);
+    hmiState.liveEventCutoff = Math.floor((Date.now() - hmiState.timeOffset - hmiState.simUpdateDelay - hmiState.liveWindow) / 1000);
+    purgeTruthEvents(hmiState);
+    updateTruthEvents(hmiState);
+    hmiState.previousUpdateTime = hmiState.currentTime;
+  }
+
   if (simUpdateTimeout) {
     clearTimeout(simUpdateTimeout);
   }
 
   //refresh layers
-  console.log("refreshing");
   for (let stat of statuses) {
     for (let animalType of animalTypes) {
       let layerName = deriveTruthLayerName(stat, animalType);
