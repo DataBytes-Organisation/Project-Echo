6366de7171506eaac5e4c5c371c9722f63e410aa
akudilcz <akudilcz@users.noreply.github.com>
2023-04-30 17:06:11 +1000

    fixing message contents

--- src/Prototypes/api/mqtt/subscriber.py
+++ src/Prototypes/api/mqtt/subscriber.py
@@ -2,23 +2,38 @@ import paho.mqtt.client as paho
 import pymongo
 import json
 import datetime
+import dotenv
 import os
+
+
+
+dotenv.load_dotenv()
+
+id = os.getenv("DB_USERNAME")
+password = os.getenv("DB_PASSWORD")
+connection_string=f"mongodb+srv://projectechodeakin:YjGHDjNsEijhytYG@cluster0.gu2idc8.mongodb.net/test"
+myclient = pymongo.MongoClient(connection_string)
+mydb = myclient["mydatabase"]
+print(myclient.list_database_names())
+# may need to empty collection to avoid doubling up on entries
+# mydb["events"].drop()
+mycol = mydb["events"]
 
 
 def on_subscribe(client, userdata, mid, granted_qos):
     print("Subscribed: "+str(mid)+" "+str(granted_qos))
 
 def on_message(client, userdata, msg):
-    # print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))    
+    print(msg.topic+" "+str(msg.qos)+" "+str(msg.payload))    
     
     json_object = json.loads(msg.payload)
-    print(json_object['timestamp'])
-
+    print(json_object)
+    mycol.insert_one(json_object)
 
 client = paho.Client()
 client.on_subscribe = on_subscribe
 client.on_message = on_message
-client.connect('localhost', 1883)
-client.subscribe('projectecho/engine/2')
+client.connect('broker.mqttdashboard.com', 1883)
+client.subscribe('projectecho/1', qos=1)
 
 client.loop_forever()
