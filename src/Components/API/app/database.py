828b8bba7c15fa735a26f55ff4e289bae876da09
Tony Zhai <tzhai@deakin.edu.au>
2023-05-08 10:55:35 +1000

    Update database.py

--- src/Components/API/app/database.py
+++ src/Components/API/app/database.py
@@ -1,6 +1,6 @@
 import pymongo
 
-# please use echonet stuff here, this connection string is just a placeholder
+# please use echonet credentials here, this connection string is just a placeholder
 connection_string="mongodb+srv://projectechodeakin:uKRBgDwBmimUuV2Q@cluster0.gu2idc8.mongodb.net/test"
 client = pymongo.MongoClient(connection_string)
 db = client['mydatabase']
