86c6a02f2f3d4559f7891c698751a9e966dc214c
akudilcz <akudilcz@users.noreply.github.com>
2023-05-11 16:13:31 +1000

    updated components using main as truth for HMI

--- src/Components/HMI/ui/server.js
+++ src/Components/HMI/ui/server.js
@@ -1,77 +1,58 @@
-//
-// Simple web server for testing stuff
-// Minosha Gamage
+const express = require('express');
+const app = express();
+const path = require('path');
 
-var port=8080;		// http port for listening
-console.log("Listening on port " + port + ".");
+const port = 8080;
 
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var querystring = require('querystring');
+// serve static files from the public directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-var requests = 0;		// keep count of requests
+//cors access to enable sending emails from different hosts
+const cors = require("cors");
+app.use(cors())
 
-var server = http.createServer(function (req,res) {
-	requests++;
-	console.log(">>> "+requests+": "+req.url);	// log the request URL
-	
-	var pathname = url.parse(req.url).pathname;  // extract the path name from the URL
-	pathname = "." + pathname;                   // public data is in same folder as server
-	
-	if (req.method == 'POST') {  				// also log HTTP POST data on console
-		var data = '';
-		req.on('data',function (d) {			// save all data events
-			data += d;
-		});
-		req.on('end', function () {				// when end of data write to console
-			console.log(data);
-		});
-	}
-	
-	// now form the HTTP response to send to the client
-	fs.stat(pathname, function (err,stat) {
-		if (err) {
-			console.log(err.code+": "+err.path);  				//       log all of the error object
-			res.writeHead(404, {'Content-type' : 'text/html'});
-			res.end();
-			return;
-		} else if (stat.isDirectory()) {
-			pathname+="/index.html";
-		}
-		fs.readFile(pathname, function(err,data) {		// does file exist?
-			if (err) { 									// NO->  bad path name
-				console.log(err.code+": "+err.path);  						//       log all of the error object
-				res.writeHead(404, {'Content-type' : 'text/html'});
-			} else {   									// YES-> file exists so send it back to client
-				if (pathname.substr(-4) == ".css" ) {
-					res.writeHead(200, {'Content-type' : 'text/css'});
-				}
-				else if (pathname.substr(-4) == ".css" ) {
-					res.writeHead(200, {'Content-type' : 'text/css'});
-				} 
-				else if (pathname.substr(-3) == ".js" ) {
-					res.writeHead(200, {'Content-type' : 'application/javascript'});
-				} 
-				else {
-					res.writeHead(200, {'Content-type' : 'text/html'});
-				}
-				res.write(data);
-			}
-		res.end();
-		});
-	});
-}).listen(port);	// listen on configured port
-//
-// print some interface information
-//
-var ipList = require('os').networkInterfaces();		// list of interfaces
-for (var device in ipList) { 						// for each device in the list of interfaces 
-   var ipData=ipList[device];
-   console.log("Interface "+device);				// header for each device	
-   for (var j=0 ; j<ipData.length; j++) {			// for each interface of device print infor
-	   console.log("    "+(ipData[j].internal ? "INTERNAL " : "EXTERNAL ")+
-	      ipData[j].family + ": " +
-		  ipData[j].address);
-   }
-}
+//bodyParser to make sure post form data is read
+const bodyParser = require("express");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//const serveIndex = require('serve-index'); 
+//app.use('/images/bio', serveIndex(express.static(path.join(__dirname, '/images/bio'))));
+
+const nodemailer = require('nodemailer');
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'echodatabytes@gmail.com',
+    pass: 'ltzoycrrkpeipngi'
+  }
+});
+
+app.post("/send_email", (req,res) => {
+  const {email, query} = req.body;
+
+  let mailOptions = {
+    from: email,
+    to: 'echodatabytes@gmail.com',
+    subject: 'Query from ' + email,
+    text: query
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+      return res.redirect("/")
+    }
+  });
+
+})
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
+
+// start the server
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
