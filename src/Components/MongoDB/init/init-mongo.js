03bacadd7b89cfa0533b0be8c0d50e5e2c9077a5
akudilcz <akudilcz@users.noreply.github.com>
2023-05-11 13:19:16 +1000

    fixed typo

--- src/Components/MongoDB/init/init-mongo.js
+++ src/Components/MongoDB/init/init-mongo.js
@@ -17,13 +17,13 @@ db.createCollection("movements");
 db.createCollection("species");
 
 const eventsData = JSON.parse(cat('/docker-entrypoint-initdb.d/events.json'));
-db.events.insertMany(speciesData);
+db.events.insertMany(eventsData);
 
 const microphonesData = JSON.parse(cat('/docker-entrypoint-initdb.d/microphones.json'));
 db.microphones.insertMany(microphonesData);
 
 const movementsData = JSON.parse(cat('/docker-entrypoint-initdb.d/movements.json'));
-db.movements.insertMany(microphonesData);
+db.movements.insertMany(movementsData);
 
 const speciesData = JSON.parse(cat('/docker-entrypoint-initdb.d/species.json'));
 db.species.insertMany(speciesData);
