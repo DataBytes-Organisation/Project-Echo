c379b82eec7a701128bb7af59f2696d35046b352
qcooke <q.cooke@deakin.edu.au>
2023-05-14 11:02:12 +1000

    Update splashscreen logo and mail vaildation

--- src/Components/HMI/ui/server.js
+++ src/Components/HMI/ui/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
 
 const port = 8080;
 
@@ -30,13 +31,27 @@ var transporter = nodemailer.createTransport({
 
 app.post("/send_email", (req,res) => {
   const {email, query} = req.body;
-
+  let html_text = '<div>';
+  html_text += '<h2>A new query has been received for Project Echo HMI</h2>'
+  html_text += '<img src="cid:logo@echo.hmi" style="height: 150px; width: 150px; display: flex; margin: auto;"/>'
+  html_text += '<p>Sender: \t ' + email + '</p>';
+  html_text += '<p>Query: \t ' + query + '</p>';
+  html_text += '<hr>';
+  html_text += '<p>Yes, this mailbox is active. So please feel free to reply to this email if you have other queries.</p>'
+  
+  html_text += '</div>';
   let mailOptions = {
     from: email,
-    to: 'echodatabytes@gmail.com',
-    subject: 'Query from ' + email,
-    text: query
-  };
+    to: `echodatabytes@gmail.com, ${email}, databytes@deakin.edu.au`,
+    subject: 'New query received!',
+    text: query,
+    html: html_text,
+    attachments: [{   // stream as an attachment
+      filename: 'image.png',
+      content: fs.createReadStream(path.join(__dirname, 'public/images/tabIcons/logo.png')),
+      cid: 'logo@echo.hmi' //same cid value as in the html
+  }]
+  }
   transporter.sendMail(mailOptions, function(error, info){
     if (error) {
       console.log(error);
