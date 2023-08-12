e53204148e4881448fea4815257a4453bd39f1b5
Prajwal Kantharaju <prajwalk930@outlook.com>
2023-08-13 09:23:55 +1000

    changes v2

--- src/Components/API/app/routers/hmi.py
+++ src/Components/API/app/routers/hmi.py
@@ -59,6 +59,7 @@ def list_species_data():
     for each_species in Species_data:
         each_species['species'] = each_species.pop('_id')
         each_species['movements'] = []
+        each_species['events'] = []
 
         Movements_data = Movements.find()
         for moves in Movements_data:
@@ -70,15 +71,15 @@ def list_species_data():
                 del moves['_id']
                 each_species['movements'].append(moves)
 
-        # events_data = Events.find()
-        # for events in events_data:
-        #     if events['species'] == each_species['species']:
+        events_data = Events.find()
+        for events in events_data:
+            if events['species'] == each_species['species']:
         #         each_species['animalId'] = events['animalId']
         #         events.pop('species')
         #         events.pop('animalId')
         #         events['id'] = str(events['_id']).replace('ObjectId', "")
         #         del events['_id']
-        #         each_species['movements'].append(events)
+                each_species['events'].append(events)
         animals_record.append(each_species)
     return animals_record
 
