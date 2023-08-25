bec923b657d3674779fe80d86e6f9b498e5e3e81
dipanshgarg21 <dipanshgarg21@gmail.com>
2023-08-25 17:44:23 +1000

    Revert "Revert "Optimized the code and added encapsulated functionality of login/signup""
    This reverts commit 1e6f43f785b14dfcd045ebe72ad78ba25cdb7881.

--- src/Components/HMI/ui/routes/auth.routes.js
+++ src/Components/HMI/ui/routes/auth.routes.js
@@ -18,20 +18,21 @@ module.exports = function (app) {
       verifySignUp.confirmPassword
     ],
     controller.signup
-  );
-
-  app.post("/api/auth/signin", (req, res) => {
-    let uname = req.body.username;
-    let re = new RegExp("^guest\_.*\_[0-9]{8,10}$");
-
-    //Check user login based on Regex Pattern:
-    if (uname.match(re) === null) {
-      console.log("username is not from Guest, proceed to User Signin")
-      controller.signin(req, res)
-    } else {
-      console.log("username is from Guest, proceed to Guest Signin")
-      controller.guestsignin(req, res)
-    }
+    );
+    
+    app.post("/api/auth/signin", (req, res) => {
+      let uname = req.body.username;
+      let re = new RegExp("^guest\_.*\_[0-9]{8,10}$");
+      
+      //Check user login based on Regex Pattern:
+      if (uname.match(re) === null) {
+        console.log("username is not from Guest, proceed to User Signin")
+        controller.signin(req, res)
+      } else {
+        console.log("username is from Guest, proceed to Guest Signin")
+        controller.guestsignin(req, res)
+      }
+      controller.signin
 
   });
 
