cf05f1c246790016d24e9fd10a66c7b0ea2155c1
FITZztif <95070150+FITZztif@users.noreply.github.com>
2023-08-29 13:32:13 +1000

    Model Integration
    Integrate the event detection, YamNet and specialized engine together

--- src/Prototypes/engine/Integration_Demo/IntegrationModel.py
+++ src/Prototypes/engine/Integration_Demo/IntegrationModel.py
@@ -15,23 +15,21 @@ from audiomentations import Compose, AddGaussianNoise, TimeStretch, PitchShift, 
 import params
 import yamnet
 
+# Set constants for audio processing
 SC = {
     'AUDIO_CLIP_DURATION': 5,  # seconds
     'AUDIO_NFFT': 2048,
     'AUDIO_WINDOW': None,
     'AUDIO_STRIDE': 200,
     'AUDIO_SAMPLE_RATE': 48000,
-    'AUDIO_MELS': 299,
+    'AUDIO_MELS': 260,
     'AUDIO_FMIN': 20,
     'AUDIO_FMAX': 13000,
     'AUDIO_TOP_DB': 80,
 
-    'MODEL_INPUT_IMAGE_WIDTH': 299,
-    'MODEL_INPUT_IMAGE_HEIGHT': 299,
+    'MODEL_INPUT_IMAGE_WIDTH': 260,
+    'MODEL_INPUT_IMAGE_HEIGHT': 260,
     'MODEL_INPUT_IMAGE_CHANNELS': 3,
-
-    'SAMPLE_VARIANTS': 20,
-    'CLASSIFIER_BATCH_SIZE': 16,
 }
 
 # Load YAMNet
@@ -44,7 +42,7 @@ model = tf.keras.models.load_model('Replace this with your own model path.')
 df = pd.read_csv('Classes.csv')
 class_names = df["Class Name"].tolist()
 
-
+# Using YamNet to recognize audio for further classification
 def recognize_audio(waveform, expected_class_names, top_n=10):
     prediction = np.mean(yamnet_model.predict(np.reshape(waveform, [1, -1]), steps=1)[0], axis=0)
     sorted_indices = np.argsort(prediction)[-top_n:][::-1]
@@ -57,6 +55,7 @@ def recognize_audio(waveform, expected_class_names, top_n=10):
         return True
     return False
 
+# Function to preprocess audio sample
 def process_audio(waveform):
     # Load and decode the audio file
     # tmp_audio_t = None
@@ -67,6 +66,7 @@ def process_audio(waveform):
     #     tmp_audio_t = tmp_audio_t.astype(np.float32)
     # sample = tmp_audio_t
 
+    # Audio clip function.
     duration_secs = SC['AUDIO_CLIP_DURATION']
     audio_duration_secs = len(waveform) / SC['AUDIO_SAMPLE_RATE']
     if audio_duration_secs > duration_secs:
@@ -81,6 +81,7 @@ def process_audio(waveform):
         subsection = np.concatenate([waveform, padding], axis=0)
     sample = subsection
 
+    # Define audio augmentations using the audiomentations library
     augmentations = Compose([
         AddGaussianNoise(min_amplitude=0.001, max_amplitude=0.015, p=0.2),
         TimeStretch(min_rate=0.8, max_rate=1.25, p=0.2),
@@ -89,6 +90,7 @@ def process_audio(waveform):
     ])
     sample = augmentations(samples=sample, sample_rate=SC['AUDIO_SAMPLE_RATE'])
 
+    # Convert audio waveform to Mel spectrogram
     image = librosa.feature.melspectrogram(
         y=sample,
         sr=SC['AUDIO_SAMPLE_RATE'],
@@ -117,7 +119,7 @@ def process_audio(waveform):
     return sample
 
 
-
+# Event detection. Automatically record the audio if the threshold is reached
 def record_and_process_audio(threshold=500):
     CHUNK = 1024
     FORMAT = pyaudio.paInt16
@@ -133,48 +135,38 @@ def record_and_process_audio(threshold=500):
 
     frames = []
     skip_next_n_frames = 0
-
     print("Listening for events...")
-
     while True:
         audio_data = stream.read(CHUNK)
-
         if skip_next_n_frames > 0:
             skip_next_n_frames -= 1
             frames.append(audio_data)
-
             if skip_next_n_frames == 0:
                 print("Processing captured audio...")
-
+                # Turn the frames into waveform
                 waveform = np.frombuffer(b"".join(frames), dtype=np.int16).astype(np.float32) / 32768.0
-
                 waveform = librosa.resample(waveform, orig_sr=RATE, target_sr=params.SAMPLE_RATE)
-
+                # Classify the sound
                 should_classify_further = recognize_audio(waveform,
                                                           ['Animal', 'Bird', 'Bird vocalization, bird call, bird song'])
-
+                # If the sound belongs to animals, using specialized engine to classify
                 if should_classify_further:
                     audio_image = process_audio(waveform)
                     audio_image = np.expand_dims(audio_image, axis=0)
-
                     predictions = model.predict(audio_image)
                     predicted_index = np.argmax(predictions[0])
                     predicted_class = class_names[predicted_index]
-
                     print(f"The predicted class is: {predicted_class}")
                 else:
                     print("The audio doesn't match the criteria for further classification.")
 
                 frames = []
             continue
-
         data_int = np.frombuffer(audio_data, dtype=np.int16)
-
         if max(data_int) > threshold:
             print("Event detected!")
             skip_next_n_frames = int((RATE / CHUNK) * 10)
             frames.append(audio_data)
-
     stream.stop_stream()
     stream.close()
     p.terminate()
