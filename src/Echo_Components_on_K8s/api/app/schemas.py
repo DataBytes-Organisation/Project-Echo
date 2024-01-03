d0b55b9d5e2f204b83db004e54870b9a711bea89
t0ru5 <bndct.dev@gmail.com>
2024-01-03 15:17:45 +1100

    Deleted old API K8S component folder

--- src/Echo_Components_on_K8s/api/app/schemas.py
+++ src/Echo_Components_on_K8s/api/app/schemas.py
@@ -104,7 +104,7 @@ class RequestSchema(BaseModel):
     initial: str
     modified: str
     source: str
-    date: str
+    date: datetime
     status: str
 
     class Config:
@@ -183,9 +183,47 @@ class UserLoginSchema(BaseModel):
         schema_extra = {
            
         } 
+class GuestSchema(BaseModel):
+    username: str
+    email: str
+    password: str
+    userId: str
+    roles: List[dict]
+    expiresAt: datetime
 
-class RoleSchema(BaseModel):
-    name: str
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+           
+        } 
+
+class GuestSignupSchema(BaseModel):
+    username: str
+    email: str
+    password: str
+    timestamp: datetime
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+           
+        } 
+
+class ForgotPasswordSchema(BaseModel):
+    user: str
+    
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
+        schema_extra = {
+           
+        } 
 
 class RecordingData(BaseModel):
     timestamp: datetime
