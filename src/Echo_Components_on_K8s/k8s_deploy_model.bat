17beb7095c3a1c739a6d7b43bafe8e8aee3ba4c6
qcooke <q.cooke@deakin.edu.au>
2024-02-18 17:12:00 +1100

    Updating batch scripts

---
+++ src/Echo_Components_on_K8s/k8s_deploy_model.bat
@@ -0,0 +1,19 @@
+set DOCKER_USER=qcooke
+set DOCKER_VERSION=v0.3
+
+echo %DOCKER_USER%
+echo %DOCKER_VERSION%
+
+echo authenticate
+docker login
+
+echo start build
+
+cd Engine
+echo build engine
+docker build -t %DOCKER_USER%/ts-model:%DOCKER_VERSION% -f %~dp0\Engine/Model.Dockerfile .
+echo push engine
+docker push %DOCKER_USER%/ts-model:%DOCKER_VERSION%
+cd ..
+
+pause
