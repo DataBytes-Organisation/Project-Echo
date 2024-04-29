fbbb35a571378daed4956a266bbba88436355fab
Tanmay Pachpande <ptanmay143@gmail.com>
2024-04-29 15:27:24 +1000

    Update training script to run on Google Colab

--- src/Prototypes/engine/Transfer Learning Models/InceptionResNetV2.py
+++ src/Prototypes/engine/Transfer Learning Models/InceptionResNetV2.py
@@ -29,26 +29,7 @@ print("Audiomentations Version  : ", audiomentations.__version__)
 
 warnings.filterwarnings("ignore")
 os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
-SC = {
-    "AUDIO_DATA_DIRECTORY": "C:\\Users\\ptanmay143\\Data\\Projects\\project-echo-data-bucket-3",
-    "CACHE_DIRETORY": "C:\\Users\\ptanmay143\\Data\\Projects\\project-echo-data-cache",
-    "AUDIO_CLIP_DURATION": 1,  # seconds
-    "AUDIO_NFFT": 2048,
-    "AUDIO_WINDOW": None,
-    "AUDIO_STRIDE": 200,
-    "AUDIO_SAMPLE_RATE": 48000,
-    "AUDIO_MELS": 260,
-    "AUDIO_FMIN": 20,
-    "AUDIO_FMAX": 13000,
-    "AUDIO_TOP_DB": 80,
-    "MODEL_INPUT_IMAGE_WIDTH": 299,
-    "MODEL_INPUT_IMAGE_HEIGHT": 299,
-    "MODEL_INPUT_IMAGE_CHANNELS": 3,
-    "USE_DISK_CACHE": True,
-    "SAMPLE_VARIANTS": 20,
-    "CLASSIFIER_BATCH_SIZE": 16,
-    "MAX_EPOCHS": 5000,
-}
+from _model_config import *
 
 
 def enforce_memory_limit(mem_mb):
