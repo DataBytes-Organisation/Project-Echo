0e8f65acd96fddaa22e8f5525af2125b634dd47b
Zohaibsajiddeakin <s223192485@deakin.edu.pk>
2024-09-10 04:00:06 +1000

    Updated middlewares

--- src/Components/HMI/ui/middleware/authJwt.js
+++ src/Components/HMI/ui/middleware/authJwt.js
@@ -1,9 +1,17 @@
 const jwt = require('jsonwebtoken');
 const config = require('../config/auth.config');
 const User = require('../model/user.model');
+
+// Define roles as constants
+const ROLES = {
+    ADMIN: 'Admin',
+    LOGGED_IN_USER: 'Logged in user',
+    GUEST: 'Guest'
+};
+
 // Middleware to verify the token and extract user role
 const verifyToken = (req, res, next) => {
-    let token = req.session.token
+    let token = req.session.token;
     
     if (!token) {
         return res.status(403).send({ message: "No token provided!" });
@@ -22,7 +30,7 @@ const verifyToken = (req, res, next) => {
 const isAdmin = async (req, res, next) => {
     try {
         const user = await User.findById(req.userId);
-        if (user.role === 'Admin') {
+        if (user.role === ROLES.ADMIN) {
             next();
         } else {
             res.status(403).send({ message: "Require Admin Role!" });
@@ -32,11 +40,11 @@ const isAdmin = async (req, res, next) => {
     }
 };
 
-// Middleware to check if the user is a Logged in user
+// Middleware to check if the user is a Logged in User
 const isLoggedInUser = async (req, res, next) => {
     try {
         const user = await User.findById(req.userId);
-        if (user.role === 'Logged in user' || user.role === 'Admin') { 
+        if (user.role === ROLES.LOGGED_IN_USER || user.role === ROLES.ADMIN) {
             next();
         } else {
             res.status(403).send({ message: "Require Logged in user Role!" });
@@ -50,10 +58,24 @@ const isLoggedInUser = async (req, res, next) => {
 const isGuest = async (req, res, next) => {
     try {
         const user = await User.findById(req.userId);
-        if (user.role === 'Guest' || user.role === 'Logged in user' || user.role === 'Admin') {
+        if (user.role === ROLES.GUEST || user.role === ROLES.LOGGED_IN_USER || user.role === ROLES.ADMIN) {
             next();
         } else {
             res.status(403).send({ message: "Require Guest Role!" });
+        }
+    } catch (err) {
+        res.status(500).send({ message: "Unable to validate user role." });
+    }
+};
+
+// Middleware to restrict access to the admin-dashboard
+const canAccessAdminDashboard = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.userId);
+        if (user.role === ROLES.ADMIN) {
+            next();
+        } else {
+            res.status(403).send({ message: "Only Admins can access the admin-dashboard!" });
         }
     } catch (err) {
         res.status(500).send({ message: "Unable to validate user role." });
@@ -64,5 +86,7 @@ module.exports = {
     verifyToken,
     isAdmin,
     isLoggedInUser,
-    isGuest
+    isGuest,
+    canAccessAdminDashboard,
+    ROLES
 };
