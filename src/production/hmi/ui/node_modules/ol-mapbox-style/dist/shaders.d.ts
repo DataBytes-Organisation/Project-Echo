1eb06553c65e110ed0c62b658f2b5e2fda28d79d
Khanh Do <152420982+AnDo081105@users.noreply.github.com>
2026-09-18 11:56:37 +1000

    chore: hmi restructure

--- src/production/hmi/ui/node_modules/ol-mapbox-style/dist/shaders.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Generates a shaded relief image given elevation data.  Uses a 3x3
- * neighborhood for determining slope and aspect.
- * @param {Array<ImageData>} inputs Array of input images.
- * @param {Object} data Data added in the "beforeoperations" event.
- * @return {ImageData} Output image.
- */
-export function hillshade(inputs: Array<ImageData>, data: any): ImageData;
